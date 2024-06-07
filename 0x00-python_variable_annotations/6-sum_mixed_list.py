7e777597ea20d2232a6beacde7c5ff56ed6ccb9e
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-07 11:53:21 +0300

    t6v0

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this is module a floor
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    method to input list of float retutn float
+    """
+    return sum(mxd_lst)
