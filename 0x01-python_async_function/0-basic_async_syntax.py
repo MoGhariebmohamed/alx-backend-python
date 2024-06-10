ba3a6c2d148c9532d6fb1413d1381d538750fce1
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-11 01:34:24 +0300

    000

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+this is module to add 2 floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    method to input 2 float and output float
+    """
+    return a + b
