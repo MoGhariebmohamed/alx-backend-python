d24767e453c1ee74e125f0e44f40c6fad96cbe1a
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-11 03:54:23 +0300

    t1v0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,7 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """
         method to delay max_delay to run
+        random() to generate random float
     """
     delay_time = random.random() * max_delay
     await asyncio.sleep(delay_time)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+this is module excersise on coroutine
+"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+        method to spawn n times delay max_delay to run
+    """
+    delay_time = [wait_random(max_delay) for _ in range(n)]
+    actual_delays = await asyncio.gather(*delay_time)
+    return sorted(actual_delays)
