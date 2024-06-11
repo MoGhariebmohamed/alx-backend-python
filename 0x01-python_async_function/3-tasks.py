3d38c9e1538741e1bde845863089e4d66a10cf08
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-11 04:53:51 +0300

    t3v1

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+this is module excersise on coroutine
+"""
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+        method to measures delay time n times delay max_delay to run
+        Args:
+        n: th tasks run
+        max_delay: the delaying rnadom max
+    """
+
+    return asyncio.creat_task(wait_random(max_delay))
