54382a9bb17e5213c8af00a22755c7861ef14752
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-11 04:17:14 +0300

    t2v1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,24 +4,15 @@ this is module excersise on coroutine
 """
 import asyncio
 import random
-import time
 from typing import List
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def measure_time(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
-        method to measures delay time n times delay max_delay to run
-        Args:
-        n: th tasks run
-        max_delay: the delaying rnadom max
+        method to spawn n times delay max_delay to run
     """
-    initiating = time.time
-    asyncio.run(wait_n(n, max_delay))
-    total time = time.time() - initiating
-    return total_time / no
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))
+    delay_time = [wait_random(max_delay) for _ in range(n)]
+    actual_delays = await asyncio.gather(*delay_time)
+    return sorted(actual_delays)
+    
