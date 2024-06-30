554e714380526699f7bf74c84d6e9f9088d13942
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-30 06:09:33 +0300

    t0v6

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,8 +57,7 @@ class TestGetJson(unittest.TestCase):
             ) -> None:
         """test the method"""
         attributes_te = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attributes_te))
-        as get_request:
+        with patch("requests.get", return_value=Mock(**attributes_te)) as get_request:
             self.assertEqual(get_json(test_url), test_payload)
             get_request.assert_called_once_with(test_url)
 
