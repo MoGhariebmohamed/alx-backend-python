abcdd11019cf2d27f1ab67ffa05bc05d9bf51865
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-30 05:38:48 +0300

    t0v2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,16 +17,19 @@ from utils import (
 class TestAccessNestedMap(unittest.TestCase):
     """the tests for nested map"""
 
-    @parameterized.expand([({"a": 1}, ("a",), 1),
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map: Dict,
+    def test_access_nested_map(
+            self, nested_map: Dict,
             path: Tuple[str],
             expected: Union[Dict, int],) -> None:
-            """test the map"""
+        """test the map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),)
+        ({"a": 1}, ("a", "b"), KeyError),
+        ])
